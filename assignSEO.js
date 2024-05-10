d57d262421bd34b5101e871c7dfa8016843acf14
Solomon Ince <53124218+Solonce@users.noreply.github.com>
2024-05-09 20:38:49 -0400

    Commit Message

---
+++ assignSEO.js
@@ -0,0 +1,24 @@
+const url = window.location.href
+const file_location = 'https://Solonce.github.io/APITestRepo/site_data.json'
+
+function setMetaTag(name, content) {
+	let metaTag = document.querySelector(`meta[name="${name}"]`);
+	if (metaTag) {
+		metaTag.content = content
+	} else {
+		metaTag = document.createElement('meta');
+		metaTag.name = name
+		metaTag.content = content
+		if(metaTag.content != ""){
+			document.head.appendChild(metaTag)
+		}
+	}
+}
+
+fetch(file_location)
+.then(response => response.json())
+.then(data => {
+	setMetaTag("keywords", data[url].keywords);
+	setMetaTag("description", data[url].description);
+})
+.catch(error => console.error("Error loading the keywords:", error));
